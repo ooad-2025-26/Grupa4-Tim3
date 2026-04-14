53e273ffeb211f6b6daaed23a8889cc1d73be27d
ddudojevic1 <ddudojevic1@etf.unsa.ba>
2026-04-14 13:57:40 +0200

    Inicijalizacija MVC projekta

---
+++ E-Sportski Centar/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using E_Sportski_Centar.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace E_Sportski_Centar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ E-Sportski Centar/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Sportski_Centar.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
